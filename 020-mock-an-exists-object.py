8a02c1cede6b077a2bb772e534c3369d95f24bf5
Neeky <1721900707@qq.com>
2023-05-27 14:36:37 +0800

    mock an exists object

--- 020-mock-an-exists-object.py
+++ 020-mock-an-exists-object.py
@@ -12,14 +12,30 @@ def tool_func():
         f.write(datetime.now().isoformat())
 
 
+class Foo(object):
+    def fun():
+        return "hello wrold"
+
+
 class TestMockAnExistsObject(unittest.TestCase):
-    def test_mock_an_exists_object(self):
+    def test_mock_an_exists_fun(self):
         """"""
         mock = MagicMock(spec=tool_func)
         mock()
         self.assertTrue(mock.called)
         mock.assert_called_once()
         mock.assert_called_once_with()
+
+    def test_mock_an_exists_object(self):
+        """ """
+        expected = "hello world"
+        mock = MagicMock(spec=Foo)
+        result = mock.fun()
+        mock.fun.assert_called_once()
+        """
+        这里的 bat 调用会失败，原因是 Foo 中并没有这个函数
+        """
+        # mock.bat()
 
 
 if __name__ == "__main__":
